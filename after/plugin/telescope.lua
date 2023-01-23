4deb602956bcd022aefac4a5c2de89d6a98c864d
Niyazi Suleymanov <niyazisuleymanov@protonmail.com>
2023-01-23 23:10:04 +0400

    use live_grep over grep_string

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input('grep: ') })
-end)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {})
