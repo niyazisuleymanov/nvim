f52cb8bd51bbd2500d2bd7fe06f12435b7017cb9
Niyazi Suleymanov <36300430+niyazisuleymanov@users.noreply.github.com>
2024-03-07 20:01:16 +0400

    add snippets

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -12,6 +12,8 @@ return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip'
 
   use {
     'nvim-telescope/telescope.nvim',
